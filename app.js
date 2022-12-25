014c01e09a02f44fc317ef9d83352e1b3d9079af
simra 123 <simrafcb@gmail.com>
2022-12-26 01:18:46 +0500

    crud done

--- app.js
+++ app.js
@@ -10,14 +10,74 @@ const client = new Client({
 	database: "pratice1",
 });
 client.connect();
-app.get("/", (req, res) => {
-	const string = "insert into jobs (company) values ('from node')";
+app.use(express.json());
+app.post("/create-job", (req, res) => {
+	// console.log(req.body, "req");
+	const { company, position, status } = req.body;
+	const string = `insert into jobs (company , position , status) values ('${company}' , '${position}' , '${status}')`;
+	if (!company || !position || !status) {
+		res.status(400).json({
+			code: 400,
+			message: "please fill all fields",
+		});
+	} else {
+		client.query(string, (err, result) => {
+			if (err) {
+				res.send(err);
+			} else {
+				res.json({
+					code: 200,
+					message: "successfully created a new job",
+				});
+				console.log(result);
+			}
+		});
+	}
+});
+app.get("/jobs", (req, res) => {
+	// console.log(req.body, "req");
+	const string = `select * from jobs `;
+
 	client.query(string, (err, result) => {
 		if (err) {
-			console.log(err);
+			res.send(err);
+		} else {
+			res.status(200).json({
+				code: 200,
+				data: result.rows,
+			});
+		}
+	});
+});
+app.get("/jobs/:id", (req, res) => {
+	const { id } = req.params;
+	const string = `select * from jobs where "Id" ='${id}'`;
+
+	client.query(string, (err, result) => {
+		if (err) {
+			res.send(err);
+		} else {
+			res.status(200).json({
+				code: 200,
+				data: result.rows,
+			});
+		}
+	});
+});
+app.post("/jobs/delete", (req, res) => {
+	const { id } = req.body;
+	const string = `delete from jobs where "Id" ='${id}'`;
+	client.query(string, (err, result) => {
+		if (err) {
+			res.send(err);
+		} else {
+			res.status(200).json({
+				code: 200,
+				message: "successfully deleted the job",
+			});
 		}
 	});
 });
 app.listen(4000, (req, res) => {
-	console.log(`server up and running`);
+	console.log(`server is up and running`);
 });
